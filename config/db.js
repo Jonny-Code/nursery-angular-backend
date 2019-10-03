d545ed906355cfe0a3224a341c7654281835d4e2
Jon Sledge <jonsledge39@gmail.com>
2019-10-03 15:32:16 -0400

    another gitignore test

--- config/db.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  database:
-    "mongodb+srv://jon:lobster123@test-cluster1-qyszp.mongodb.net/test?retryWrites=true&w=majority",
-  // database: "mongodb://localhost:27017/nurseryauth",
-  secret: "thesecret"
-};
